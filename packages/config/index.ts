ec9eb7b09e8a2af0766bbd6f8a7ed56117d2d111
Neal Uehara <ueharaneal@gmail.com>
2025-04-04 22:26:22 -0700

    got the server and web to run concurrently

---
+++ packages/config/index.ts
@@ -0,0 +1,3 @@
+// This file exists to satisfy TypeScript's requirement for at least one input file
+// It is not meant to be used directly, but rather to allow the tsconfig.json to be valid
+export {};
