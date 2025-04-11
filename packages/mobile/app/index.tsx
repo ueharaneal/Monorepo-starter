e360eedeea00038d6ae2ed7693cd604778d47b5d
Neal Uehara <ueharaneal@gmail.com>
2025-04-10 22:34:00 -0700

    fixed the stupid auth load

--- packages/mobile/app/index.tsx
+++ packages/mobile/app/index.tsx
@@ -1,6 +1,29 @@
 import { StyleSheet, Text, View } from "react-native";
+import { Redirect } from "expo-router";
+import { useAuth } from "@/providers/AuthProvider";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect } from "react";
+export function unstable_settings() {
+  return { auth: false };
+}
 
 export default function Page() {
+  const { session, isLoading } = useAuth();
+
+  useEffect(() => {
+    if (!isLoading) {
+      SplashScreen.hideAsync();
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (!session) {
+    return <Redirect href="/(auth)/signup" />;
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.main}>
